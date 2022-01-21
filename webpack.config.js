f3495c879d9038b18055ba8189c939391837c63d
rania333 <raniaaramadan1999@gmail.com>
2022-01-21 23:26:19 +0200

    webpack task

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const myPath = require('path');
+const htmlwebpack = require('html-webpack-plugin');//de bt3ml html file
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //instead of style-loader
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+    entry:"./src/index.js", //l entry point
+    output: {
+        path: myPath.resolve(__dirname, 'dist'),//dist path
+        filename: 'myBundle.js',//file inside dist folder
+        assetModuleFilename:'images/[name][ext]'//da path l images x el-production
+    },
+    mode: 'development',
+    plugins: [
+        new CssMinimizerPlugin(),
+        new MiniCssExtractPlugin({filename: 'style/myStyle.css'}),
+        new htmlwebpack({template: 'src/index.html'}),
+
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i, //da loader ll css
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                //l mini de bdl l style-loader
+            }, 
+            { //loader ll images-> built-in loader
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // 'style-loader',
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ]
+    },
+    
+    // optimization:{
+    //     minimize:true,
+    //     minimizer:["...", new ImageMinimizerPlugin({
+    //         minimizerOptions: {
+    //             plugins: [
+    //                 ["gifsicle", { interlaced: true }],
+    //                 ["mozjpeg",{quality:70}],
+    //                 ["optipng", { optimizationLevel: 5 }],
+    //                 ['svgo']
+    //             ],
+    //         },
+    //     }),
+    // ]
+    // }
+}
